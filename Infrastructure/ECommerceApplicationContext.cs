aae8ea75248acf1dca33c6f7359e91e2b6e86ffe
Robert-Alexandru Zaharia <alex_robert02@yahoo.com>
2023-11-07 15:04:13 +0200

    + domain and infrastructure

---
+++ Infrastructure/ECommerceApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using ECommerceApplication.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class ECommergeApplicationContext : DbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=ECommergeApplicationDB;Username=postgres;Password=password");
+        }
+    }
+}
