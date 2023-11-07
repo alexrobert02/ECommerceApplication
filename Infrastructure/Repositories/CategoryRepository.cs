fa959487344584c592cdfc4494e5aa50d9f79cca
Robert-Alexandru Zaharia <alex_robert02@yahoo.com>
2023-11-07 18:53:18 +0200

    + repositories

---
+++ Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using ECommerceApplication.Application.Persistence;
+using ECommerceApplication.Domain.Entities;
+
+namespace Infrastructure.Repositories
+{
+    public class CategoryRepository : BaseRepository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(ECommergeApplicationContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using ECommerceApplication.Domain.Entities;
+using Infrastructure;
+using Infrastructure.Repositories;
+
+Console.WriteLine("Hello, World!");
+
+Category category = Category.Create("Gardening").Value;
+ECommergeApplicationContext context = new();
+CategoryRepository repository = new(context);
+await repository.AddAsync(category);
+var result = await repository.FindByIdAsync(category.CategoryId);
+Console.WriteLine(result.Value.CategoryId);
