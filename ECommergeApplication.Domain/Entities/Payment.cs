1a031ec15363ad82ec389230b6c3c571f049ad03
Ana-Maria <95433627+Ana-Maria-C@users.noreply.github.com>
2023-11-07 15:32:18 +0200

    +some entities

---
+++ ECommergeApplication.Domain/Entities/Payment.cs
@@ -0,0 +1,37 @@
+﻿using ECommerceApplication.Domain.Common;
+using ECommerceApplication.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApplication.Domain.Entities
+{
+    public class Payment : AuditableEntity
+    {
+        private Payment(Guid paymentId, Guid orderId, decimal amount, DateTime paymentDate, string paymentMethod)
+        {
+            PaymentId = paymentId;
+            OrderId = orderId;
+            Amount = amount;
+            PaymentDate = paymentDate;
+            PaymentMethod = paymentMethod;
+        }
+        public Guid PaymentId { get; private set; }
+        public Guid OrderId { get; private set; }
+        public decimal Amount { get; private set; }
+        public DateTime PaymentDate { get; private set; }
+        public string PaymentMethod { get; private set; }
+        public string PaymentStatus { get; private set; }
+
+        public bool IsPaymentValid()
+        {
+            if (PaymentStatus == "Paid")
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ ECommergeApplication.Domain/Entities/Product.cs
@@ -0,0 +1,59 @@
+﻿using ECommerceApplication.Domain.Common;
+using ECommerceApplication.Domain.Common;
+
+namespace ECommerceApplication.Domain.Entities
+{
+    public class Product : AuditableEntity
+    {
+        private Product(string productName, decimal price)
+        {
+            ProductId = Guid.NewGuid();
+            ProductName = productName;
+            Price = price;
+        }
+        public Guid ProductId { get; private set; }
+        public string ProductName { get; private set; } = string.Empty;
+        public decimal Price { get; private set; }
+        public string? Description { get; private set; }
+        public string? ImageUrl { get; private set; }
+
+        public static Result<Product> Create(string productName, decimal price)
+        {
+            if (string.IsNullOrWhiteSpace(productName))
+            {
+                return Result<Product>.Failure("Product Name is required.");
+            }
+            if (price <= 0)
+            {
+                return Result<Product>.Failure("Price must be greater than zero.");
+            }
+            return Result<Product>.Success(new Product(productName, price));
+        }
+        public Guid CategoryId { get; private set; }
+
+        public void AttachDescription(string description)
+        {
+            if (!string.IsNullOrWhiteSpace(description))
+            {
+                Description = description;
+            }
+        }
+
+        public void AttachImageUrl(string imageUrl)
+        {
+            if (!string.IsNullOrWhiteSpace(imageUrl))
+            {
+                ImageUrl = imageUrl;
+            }
+        }
+
+        public void AttachCategory(Guid categoryId)
+        {
+            if (categoryId != Guid.Empty)
+            {
+                CategoryId = categoryId;
+            }
+        }
+
+    }
+}
