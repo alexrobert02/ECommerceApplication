50d0fcf56c04d4a3b86ca2c7d62e1ccf9a4ac03f
Ana-Maria <95433627+Ana-Maria-C@users.noreply.github.com>
2023-11-08 19:12:01 +0200

    + decimal rating

--- ECommerceApplication.Application/Persistence/IProductRepository.cs
+++ ECommerceApplication.Application/Persistence/IProductRepository.cs
@@ -1,9 +1,4 @@
 ﻿using ECommerceApplication.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ECommerceApplication.Application.Persistence
 {
--- Infrastructure/Repositories/ReviewRepository.cs
+++ Infrastructure/Repositories/ReviewRepository.cs
@@ -1,10 +1,5 @@
 ﻿using ECommerceApplication.Application.Persistence;
 using ECommerceApplication.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Infrastructure.Repositories
 {
