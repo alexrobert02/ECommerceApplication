5cb71989956b31c313dc9cb1fe1e2d1f8a400894
Ana-Maria <95433627+Ana-Maria-C@users.noreply.github.com>
2023-11-08 19:12:01 +0200

    + decimal rating

--- Infrastructure/Repositories/ProductRepository.cs
+++ Infrastructure/Repositories/ProductRepository.cs
@@ -1,15 +1,10 @@
 ﻿using ECommerceApplication.Application.Persistence;
 using ECommerceApplication.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Infrastructure.Repositories
 {
-  
-        public class ProductRepository : BaseRepository<Product>, IProductRepository
+
+    public class ProductRepository : BaseRepository<Product>, IProductRepository
         {
             public ProductRepository(ECommergeApplicationContext context) : base(context)
             {
