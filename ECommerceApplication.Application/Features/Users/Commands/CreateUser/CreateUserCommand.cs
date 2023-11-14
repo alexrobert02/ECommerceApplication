b35d9e461d7b9fd7c659c2147650e4d55569c832
Robert-Alexandru Zaharia <alex_robert02@yahoo.com>
2023-11-14 17:20:39 +0200

    .

---
+++ ECommerceApplication.Application/Features/Users/Commands/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace ECommerceApplication.Application.Features.Users.Commands.CreateUser
+{
+    public class CreateUserCommand : IRequest<CreateUserCommandResponse>
+    {
+        public string Username { get; private set; } = default!;
+        public string Email { get; private set; } = default!;
+        public string PasswordHash { get; private set; } = default!;
+        public string FirstName { get; private set; } = default!;
+        public string LastName { get; private set; } = default!;
+        public string Address { get; private set; } = default!;
+        public string PhoneNumber { get; private set; } = default!;
+    }
+}
---
+++ ECommerceApplication.Application/Features/Users/Commands/CreateUser/CreateUserCommandHandler.cs
@@ -0,0 +1,58 @@
+﻿using ECommerceApplication.Application.Persistence;
+using ECommerceApplication.Domain.Entities;
+using MediatR;
+
+namespace ECommerceApplication.Application.Features.Users.Commands.CreateUser
+{
+    public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, CreateUserCommandResponse>
+    {
+        private readonly IUserRepository repository;
+
+        public CreateUserCommandHandler(IUserRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<CreateUserCommandResponse> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+        {
+            var validator = new CreateUserCommandValidator();
+            var validatorResult = await validator.ValidateAsync(request, cancellationToken);
+
+            if (!validatorResult.IsValid)
+            {
+                return new CreateUserCommandResponse
+                {
+                    Success = false,
+                    ValidationsErrors = validatorResult.Errors.Select(e => e.ErrorMessage).ToList()
+                };
+            }
+            var user = User.Create(request.Username, request.Email, request.PasswordHash, request.FirstName, request.LastName, request.Address, request.PhoneNumber);
+            if (!user.IsSuccess)
+            {
+                return new CreateUserCommandResponse
+                {
+                    Success = false,
+                    ValidationsErrors = new List<string> { user.Error }
+                };
+            }
+
+            await repository.AddAsync(user.Value);
+
+            return new CreateUserCommandResponse
+            {
+                Success = true,
+                User = new CreateUserDto
+                {
+                    UserId = user.Value.UserId,
+                    Username = user.Value.Username,
+                    Email = user.Value.Email,
+                    PasswordHash = user.Value.PasswordHash,
+                    FirstName = user.Value.FirstName,
+                    LastName = user.Value.LastName,
+                    Address = user.Value.Address,
+                    PhoneNumber = user.Value.PhoneNumber
+                }
+            };
+        }
+    }
+}
