cd353d93f37c0009a3523fdb00dd1961c54b9cbd
Robert-Alexandru Zaharia <alex_robert02@yahoo.com>
2023-11-14 15:28:55 +0200

    + getbyid

---
+++ ECommerceApplication.Application/Features/Categories/Commands/GetByIdCategory/GetByIdCategoryCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace ECommerceApplication.Application.Features.Categories.Commands.GetByIdCategory
+{
+    public class GetByIdCategoryCommand : IRequest<GetByIdCategoryCommandResponse>
+    {
+        public Guid CategoryId { get; set; } = default!;
+    }
+}
---
+++ ECommerceApplication.Application/Features/Categories/Commands/GetByIdCategory/GetByIdCategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ECommerceApplication.Application.Features.Categories.Commands.GetByIdCategory
+{
+    public class GetByIdCategoryDto
+    {
+        public Guid CategoryId { get; set; }
+        public string? CategoryName { get; set; }
+    }
+}
