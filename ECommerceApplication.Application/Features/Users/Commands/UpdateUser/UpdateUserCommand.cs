df1cef26a08f3047353967cf3bfc7b326d8fadab
Robert-Alexandru Zaharia <alex_robert02@yahoo.com>
2023-11-14 23:01:04 +0200

    + put user

---
+++ ECommerceApplication.Application/Features/Users/Commands/UpdateUser/UpdateUserCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace ECommerceApplication.Application.Features.Users.Commands.UpdateUser
+{
+    public class UpdateUserCommand : IRequest<UpdateUserCommandResponse>
+    {
+        public Guid UserId { get; set; }
+        public string Username { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public string PasswordHash { get; set; } = default!;
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+        public string Address { get; set; } = default!;
+        public string PhoneNumber { get; set; } = default!;
+    }
+}
---
+++ ECommerceApplication.Application/Features/Users/Commands/UpdateUser/UpdateUserCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace ECommerceApplication.Application.Features.Users.Commands.UpdateUser
+{
+    public class UpdateUserCommandValidator : AbstractValidator<UpdateUserCommand>
+    {
+        public UpdateUserCommandValidator()
+        {
+            RuleFor(p => p.UserId)
+                .NotEmpty().WithMessage("{PropertyName} is required.");
+
+            // Add validation rules for other properties if needed
+            // ...
+        }
+    }
+}
