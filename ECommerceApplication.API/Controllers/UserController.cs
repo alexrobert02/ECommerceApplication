a6902aa7d73fbcf96eb38fd14868c2f8c21eed62
Robert-Alexandru Zaharia <alex_robert02@yahoo.com>
2023-11-14 17:20:39 +0200

    .

---
+++ ECommerceApplication.API/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using ECommerceApplication.Application.Features.Users.Commands.CreateUser;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceApplication.API.Controllers
+{
+    public class UserController : ApiControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> Create(CreateUserCommand command)
+        {
+            var result = await Mediator.Send(command);
+            if (!result.Success)
+            {
+                return BadRequest(result);
+            }
+            return Ok(result);
+        }
+    }
+}
---
+++ ECommerceApplication.Application/Features/Users/Commands/CreateUser/CreateUserCommandValidator.cs
@@ -0,0 +1,39 @@
+﻿using FluentValidation;
+
+namespace ECommerceApplication.Application.Features.Users.Commands.CreateUser
+{
+    public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
+    {
+        public CreateUserCommandValidator()
+        {
+            RuleFor(p => p.Username)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotNull()
+                .MaximumLength(100).WithMessage("{PropertyName} must not exceed 100 characters.");
+            RuleFor(p => p.Email)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotNull()
+                .MaximumLength(100).WithMessage("{PropertyName} must not exceed 100 characters.");
+            RuleFor(p => p.PasswordHash)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotNull()
+                .MaximumLength(100).WithMessage("{PropertyName} must not exceed 100 characters.");
+            RuleFor(p => p.FirstName)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotNull()
+                .MaximumLength(100).WithMessage("{PropertyName} must not exceed 100 characters.");
+            RuleFor(p => p.LastName)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotNull()
+                .MaximumLength(100).WithMessage("{PropertyName} must not exceed 100 characters.");
+            RuleFor(p => p.Address)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotNull()
+                .MaximumLength(100).WithMessage("{PropertyName} must not exceed 100 characters.");
+            RuleFor(p => p.PhoneNumber)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotNull()
+                .MaximumLength(100).WithMessage("{PropertyName} must not exceed 100 characters.");
+        }
+    }
+}
