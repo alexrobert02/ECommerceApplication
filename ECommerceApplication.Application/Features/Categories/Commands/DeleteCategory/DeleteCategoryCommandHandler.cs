a356b39fdd6ea3bf0543be39a95d45673fe08169
Ana-Maria <95433627+Ana-Maria-C@users.noreply.github.com>
2023-11-14 19:05:18 +0200

    +delete_category

---
+++ ECommerceApplication.Application/Features/Categories/Commands/DeleteCategory/DeleteCategoryCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using ECommerceApplication.Application.Persistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApplication.Application.Features.Categories.Commands.DeleteCategory
+{
+    public class DeleteCategoryCommandHandler : IRequestHandler <DeleteCategoryCommand, DeleteCategoryCommandResponse>
+    {
+       private readonly ICategoryRepository _categoryRepository;
+        public DeleteCategoryCommandHandler(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+        public async Task<DeleteCategoryCommandResponse> Handle(DeleteCategoryCommand request, CancellationToken cancellationToken)
+        {
+            var response = new DeleteCategoryCommandResponse();
+            var category = await _categoryRepository.FindByIdAsync(request.CategoryId);
+            if (category == null)
+            {
+                response.Success = false;
+                response.Message = "Category not found";
+                return response;
+            }
+            await _categoryRepository.DeleteAsync(category.Value.CategoryId);
+            response.Success = true;
+            response.Message = "Category deleted successfully";
+            return response;
+        }
+    }
+}
---
+++ ECommerceApplication.Application/Features/Categories/Commands/DeleteCategory/DeleteCategoryCommandResponse.cs
@@ -0,0 +1,17 @@
+﻿using ECommerceApplication.Application.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApplication.Application.Features.Categories.Commands.DeleteCategory
+{
+    public class DeleteCategoryCommandResponse : BaseResponse
+    {
+        public DeleteCategoryCommandResponse() : base()
+        {
+        }
+        public DeleteCategoryDto Category { get; set; }
+    }
+}
