a9711c2798f085215f53cdab80cb5525e6a6bbd6
Robert-Alexandru Zaharia <alex_robert02@yahoo.com>
2023-12-05 20:28:13 +0200

    + delete for products

---
+++ ECommerceApplication.Application/Features/Products/Commands/DeleteProduct/DeleteProductCommandResponse.cs
@@ -0,0 +1,8 @@
+﻿using ECommerceApplication.Application.Responses;
+
+namespace ECommerceApplication.Application.Features.Products.Commands.DeleteProduct
+{
+    public class DeleteProductCommandResponse : BaseResponse
+    {
+    }
+}
