1b6ea1cc1dd6186806998c97c9fdf34348850d1e
Robert-Alexandru Zaharia <alex_robert02@yahoo.com>
2023-12-05 21:36:31 +0200

    + autorizare

--- ECommerceApplication.Identity/Migrations/20231205184703_InitialCreate.Designer.cs
+++ ECommerceApplication.Identity/Migrations/20231205184703_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ECommerceApplication.Identity.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20231205104345_InitialCreate")]
+    [Migration("20231205184703_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
