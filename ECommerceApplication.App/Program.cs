b2dafd64db192a7e53440087f5589027ebe1e25d
Achitei Marius <marius.achitei02@gmail.com>
2023-12-11 09:10:42 +0200

    nu prea

--- ECommerceApplication.App/Program.cs
+++ ECommerceApplication.App/Program.cs
@@ -1,11 +1,40 @@
+using Blazored.LocalStorage;
+using ECommerceApplication.App.Auth;
+using ECommerceApplication.App.Contracts;
 using ECommerceApplication.App;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using ECommerceApplication.App.Services;
+using ECommerceApplication.TicketManagement.App.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddAuthorizationCore();
+builder.Services.AddBlazoredLocalStorage(config =>
+{
+    config.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+    config.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+    config.JsonSerializerOptions.IgnoreReadOnlyProperties = true;
+    config.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+    config.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+    config.JsonSerializerOptions.ReadCommentHandling = JsonCommentHandling.Skip;
+    config.JsonSerializerOptions.WriteIndented = false;
+});
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<CustomStateProvider>();
+builder.Services.AddHttpClient<ICategoryDataService, CategoryDataService>(client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7165/");
+});
+builder.Services.AddScoped<AuthenticationStateProvider>(s => s.GetRequiredService<CustomStateProvider>());
+builder.Services.AddHttpClient<IAuthenticationService, AuthenticationService>(client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7165/");
+});
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
---
+++ ECommerceApplication.App/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace ECommerceApplication.App.ViewModels
+{
+    public class CategoryViewModel
+    {
+        public Guid CategoryId { get; set; }
+        [Required(ErrorMessage = "Category name is required")]
+        public string CategoryName { get; set; } = string.Empty;
+    }
+}
