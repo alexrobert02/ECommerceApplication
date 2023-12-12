60a5b44c5fd6a5e64cccb399af70aa29952dc18e
Robert-Alexandru Zaharia <alex_robert02@yahoo.com>
2023-12-12 14:29:09 +0200

    + got rid of name

--- ECommerceApplication.App/ViewModels/RegisterViewModel.cs
+++ ECommerceApplication.App/ViewModels/RegisterViewModel.cs
@@ -3,7 +3,6 @@
     public class RegisterViewModel
     {
         public string Username { get; set; } = string.Empty;
-        public string Name { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
     }
