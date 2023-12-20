4be9f6979bc4f429305a69f6e6897658786f22c5
Ana-Maria <95433627+Ana-Maria-C@users.noreply.github.com>
2023-12-20 17:20:05 +0200

    +some test for product

---
+++ ECommercerApplication.Application.Tests/Queries/ProductTests/GetAllProductQueryHandlerTests.cs
@@ -0,0 +1,43 @@
+﻿
+using ECommerceApplication.Application.Features.Products.Queries.GetAllProduct;
+using NSubstitute;
+using ECommerceApplication.Application.Persistence;
+using ECommerceApplication.Domain.Entities;
+
+using ECommerceApplication.Domain.Common;
+
+namespace ECommercerApplication.Application.Tests.Queries.ProductTests
+{
+    public class GetAllProductQueryHandlerTests
+    {
+        private readonly IProductRepository _repository;
+        private readonly GetAllProductQueryHandler _handler;
+
+        public GetAllProductQueryHandlerTests()
+        {
+            _repository = Substitute.For<IProductRepository>();
+            _handler = new GetAllProductQueryHandler(_repository);
+        }
+
+        [Fact]
+        public async Task Handle_ReturnAllProducts()
+        {
+            var product = new List<Product>
+            {
+                Product.Create("Product 1",10).Value,
+                Product.Create("Product 2", 2).Value
+            };
+
+            var result = Result<IReadOnlyList<Product>>.Success(product);
+            _repository.GetAllAsync().Returns(Task
+                               .FromResult<Result<IReadOnlyList<Product>>>(result));
+            var response = await _handler.Handle(new GetAllProductQuery(), new CancellationToken());
+            Assert.NotNull(response);
+            Assert.Equal(product.Count, response.Products.Count);
+            Assert.All(response.Products, productDto =>
+                           Assert.Contains(product, c => c.ProductId == productDto.ProductId && c.ProductName == productDto.ProductName && c.Price == productDto.Price));
+        }
+
+
+    }
+}
