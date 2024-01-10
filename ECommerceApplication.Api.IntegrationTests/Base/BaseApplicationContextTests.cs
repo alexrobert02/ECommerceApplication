cbb6e957585dd95e79fe73f7aa9490c1ec212a30
Robert-Alexandru Zaharia <alex_robert02@yahoo.com>
2024-01-10 12:41:11 +0200

    + tests for api integration

---
+++ ECommerceApplication.Api.IntegrationTests/Base/BaseApplicationContextTests.cs
@@ -0,0 +1,64 @@
+﻿using ECommerceApplication.Infrastructure;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+
+namespace ECommerceApplication.API.IntegrationTests.Base
+{
+    public abstract class BaseApplicationContextTests : IAsyncDisposable
+    {
+        protected readonly WebApplicationFactory<Program> Application;
+        protected readonly HttpClient Client;
+
+        protected BaseApplicationContextTests()
+        {
+            Application = new WebApplicationFactory<Program>();
+            Application = Application.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                    services.RemoveAll(typeof(DbContextOptions<ECommerceApplicationContext>));
+                    services.AddDbContext<ECommerceApplicationContext>(options =>
+                    {
+                        options.UseInMemoryDatabase("ECommerceApplicationDbForTesting");
+                    });
+                                        
+                    services.Configure<JwtBearerOptions>(
+                        JwtBearerDefaults.AuthenticationScheme,
+                        options =>
+                        {
+                            options.Configuration = new OpenIdConnectConfiguration
+                            {
+                                Issuer = JwtTokenProvider.Issuer,
+                            };
+                            options.TokenValidationParameters.ValidIssuer = JwtTokenProvider.Issuer;
+                            options.TokenValidationParameters.ValidAudience = JwtTokenProvider.Issuer;
+                            options.Configuration.SigningKeys.Add(JwtTokenProvider.SecurityKey);
+                        }
+                    );
+                    var sp = services.BuildServiceProvider();
+                    using (var scope = sp.CreateScope())
+                    {
+                        var scopedServices = scope.ServiceProvider;
+                        var db = scopedServices.GetRequiredService<ECommerceApplicationContext>();
+
+                        db.Database.EnsureDeleted();
+                        db.Database.EnsureCreated();
+
+                        Seed.InitializeDbForTests(db);
+                    }
+                });
+            });
+            Client = Application.CreateClient();
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            GC.SuppressFinalize(this);
+            await Application.DisposeAsync();
+        }
+    }
+}
