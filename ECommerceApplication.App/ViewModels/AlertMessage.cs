4190da81bbb1e90a310643ee863070b7c437ed53
Achitei Marius <marius.achitei02@gmail.com>
2024-01-15 00:40:20 +0200

    delete orderItem

---
+++ ECommerceApplication.App/ViewModels/AlertMessage.cs
@@ -0,0 +1,11 @@
+﻿namespace ECommerceApplication.App.ViewModels
+{
+    public class AlertMessage
+    {
+        public string Message { get; set; }
+        public bool IsSuccess { get; set; }
+        public bool IsEnabled { get; set; }
+
+        public string type { get; set; }
+    }
+}
