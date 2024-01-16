795ce29b1dd706de558168b5226bf17f219175ad
Robert-Alexandru Zaharia <alex_robert02@yahoo.com>
2024-01-16 19:53:14 +0200

    + getproductbycategoryid

---
+++ ECommerceApplication.Application/Features/Products/Queries/GetProductByCategoryId/GetProductByCategoryIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace ECommerceApplication.Application.Features.Products.Queries.GetProductByCategoryId
+{
+    public class GetProductByCategoryIdQuery : IRequest<GetProductByCategoryIdResponse>
+    {
+        public Guid CategoryId { get; set; } = default!;
+    }
+}
--- ECommerceApplication.Application/Persistence/IProductRepository.cs
+++ ECommerceApplication.Application/Persistence/IProductRepository.cs
@@ -1,9 +1,12 @@
-﻿using ECommerceApplication.Domain.Entities;
+﻿using ECommerceApplication.Domain.Common;
+using ECommerceApplication.Domain.Entities;
 
 namespace ECommerceApplication.Application.Persistence
 {
     public interface IProductRepository : IAsyncRepository<Product>
     {
         Task<bool> ProductExists(Guid productId);
+
+        Task<Result<List<Product>>> GetProductByCategoryIdAsync(Guid userId);
     }
 }
