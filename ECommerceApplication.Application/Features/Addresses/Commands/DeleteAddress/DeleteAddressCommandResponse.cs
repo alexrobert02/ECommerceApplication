39aebd8b3f0deb3e625ec018f239e95c81a14b5a
Ana-Maria <95433627+Ana-Maria-C@users.noreply.github.com>
2024-01-17 16:18:35 +0200

    +addresses page

---
+++ ECommerceApplication.Application/Features/Addresses/Commands/DeleteAddress/DeleteAddressCommandResponse.cs
@@ -0,0 +1,13 @@
+﻿using ECommerceApplication.Application.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApplication.Application.Features.Addresses.Commands.DeleteAddress
+{
+    public class DeleteAddressCommandResponse:BaseResponse
+    {
+    }
+}
